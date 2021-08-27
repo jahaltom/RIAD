93b32f48b2caab84d72d7be5796ffbaa05757e1a
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2021-08-27 18:25:44 -0500

    Update VarCall.py

--- VarCall.py
+++ VarCall.py
@@ -4,17 +4,13 @@ DIR='output'
 #Set path to human genome
 
 
-chr_list = list(range(1, 23))
-chr_list.append("X")
 
 
 #Read in run accession IDs from txt file. 
 with open ("RAids.txt") as f:
     ra=f.read().splitlines()
 
-wildcard_constraints:
-    chr= '|'.join([str(x) for x in chr_list]),
-     	
+
 
 rule all:
 	input: expand("{wd}/{sample}/vcf.gz".format(sample=s,wd=DIR) for s in ra)  
@@ -24,14 +20,22 @@ rule var_call:
         [directory("{wd}/{sample}").format(wd=DIR,sample=s) for s in ra]
     output: "{wd}/{sample}/vcf.gz"
     shell:
-        """   
+        """
+        #Removes all but 1KG Chrs
+        #samtools index -b {wildcards.wd}/{wildcards.sample}/Aligned.sortedByCoord.out_star.bam
+        #samtools view -b {wildcards.wd}/{wildcards.sample}/Aligned.sortedByCoord.out_star.bam {{1..22}} X  > {wildcards.wd}/{wildcards.sample}/output.bam
+
+        
+        
+    
+
         gatk MarkDuplicates \
             I= {wildcards.wd}/{wildcards.sample}/Aligned.sortedByCoord.out_star.bam \
             O= {wildcards.wd}/{wildcards.sample}/MarkDup.bam \
             CREATE_INDEX= true \
             METRICS_FILE= {wildcards.wd}/{wildcards.sample}/marked_dup_metrics.txt \
             VALIDATION_STRINGENCY= SILENT
-        rm {wildcards.wd}/{wildcards.sample}/Aligned.sortedByCoord.out_star.bam    
+            
         gatk AddOrReplaceReadGroups \
             I= {wildcards.wd}/{wildcards.sample}/MarkDup.bam \
             O= {wildcards.wd}/{wildcards.sample}/Grouped.bam \
@@ -40,25 +44,30 @@ rule var_call:
             RGPL= plat \
             RGPU= plat
         rm {wildcards.wd}/{wildcards.sample}/Mark*
-    
+
+
+   
         gatk SplitNCigarReads \
             -I {wildcards.wd}/{wildcards.sample}/Grouped.bam \
             -O {wildcards.wd}/{wildcards.sample}/final.bam \
-            -R /ocean/projects/mcb200036p/jahaltom/Ancestry/data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+            -R /work/LAS/xgu-lab/Ancestry/data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna 
+          
             
         rm {wildcards.wd}/{wildcards.sample}/Grouped*
         
         gatk --java-options "-Xmx20g" HaplotypeCaller \
            -I {wildcards.wd}/{wildcards.sample}/final.bam \
            -O {wildcards.wd}/{wildcards.sample}/vcf.gz \
-           -R /ocean/projects/mcb200036p/jahaltom/Ancestry/data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
-           -L /ocean/projects/mcb200036p/jahaltom/Ancestry/data/ChrAllIntervals.interval_list \
+           -R /work/LAS/xgu-lab/Ancestry/data/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
+           -L /work/LAS/xgu-lab/Ancestry/data/ChrAll.interval_list \
            -ERC GVCF \
-           --native-pair-hmm-threads 16       
-        
+           --native-pair-hmm-threads 16  
         rm {wildcards.wd}/{wildcards.sample}/final*
+        
+       
         gunzip -c {wildcards.wd}/{wildcards.sample}/vcf.gz |  grep -v "0/0:0:0:0:0,0,0" > {wildcards.wd}/{wildcards.sample}/vcf
         rm {wildcards.wd}/{wildcards.sample}/vcf.gz*
         bgzip {wildcards.wd}/{wildcards.sample}/vcf        
-        bcftools index -t {wildcards.wd}/{wildcards.sample}/vcf.gz  
+        bcftools index -t {wildcards.wd}/{wildcards.sample}/vcf.gz
         """
+#rm {wildcards.wd}/{wildcards.sample}/Aligned.sortedByCoord.out_star.bam
