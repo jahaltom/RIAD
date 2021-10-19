6eae08ecf75d73dbbc8024d27f8ac70bfb9acd61
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2021-10-18 20:52:52 -0500

    Update DEseq2.r

--- DEseq2.r
+++ DEseq2.r
@@ -6,6 +6,9 @@ countData = read.table("JH-2021-6-11-Counts-Mason-Nasal.tsv",header=TRUE,row.nam
 countData=countData[!duplicated(countData[ , c("Gene_stable_ID")]),]
 ##Read in expermental design
 colData = read.table("Design",header=TRUE,row.names=NULL,sep = '\t')
+colData=colData[colData$RT_PCR_Result=="Detected",]
+
+
 
 #Get sample names 
 samples=as.character(colData$samples)
@@ -18,8 +21,9 @@ rownames(counts)=countData$Gene_stable_ID
 
 
 
+
 ##Bring everything together 
-dds = DESeqDataSetFromMatrix(countData = counts,colData = colData,design = ~ Ancestry + RT_PCR_Result)
+dds = DESeqDataSetFromMatrix(countData = counts,colData = colData,design = ~ Ancestry)
 
 dds <- estimateSizeFactors(dds)
 idx <- rowSums( counts(dds, normalized=TRUE) >= 30 ) >= 10
@@ -27,6 +31,66 @@ dds <- dds[idx,]
 dds <- DESeq(dds)
 
 
-result = results(dds, contrast=c(Ancestry,EUR,AFR))
+result = results(dds, contrast=c("Ancestry","EUR","AFR"))
 result = result[complete.cases(result),]## to remove rows with NA
-write.table(result, TumorDEG.txt)
+write.table(result, "EUR_AFR_DEG.txt")
+
+
+result = results(dds, contrast=c("Ancestry","EUR","SAS"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "EUR_SAS_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","EUR","EAS"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "EUR_EAS_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","EUR","AMR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "EUR_AMR_DEG.txt")
+
+
+
+
+
+
+result = results(dds, contrast=c("Ancestry","EAS","AFR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "EAS_AFR_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","SAS","AFR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "SAS_AFR_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","AMR","AFR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "AMR_AFR_DEG.txt")
+
+
+
+result = results(dds, contrast=c("Ancestry","SAS","EAS"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "SAS_EAS_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","SAS","AMR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "SAS_AMR_DEG.txt")
+
+result = results(dds, contrast=c("Ancestry","EAS","AMR"))
+result = result[complete.cases(result),]## to remove rows with NA
+write.table(result, "EAS_AMR_DEG.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
