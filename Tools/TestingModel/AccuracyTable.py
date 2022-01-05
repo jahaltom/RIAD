ae47b97d03d98e066dec2c4cfb5799a7e464778f
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-01-05 17:53:28 -0600

    Update AccuracyTable.py

--- Tools/TestingModel/AccuracyTable.py
+++ Tools/TestingModel/AccuracyTable.py
@@ -9,7 +9,6 @@ Super=['SuperpopulationChrAll.PC20SVMResults',  'SuperpopulationChrAll.PC20Rando
 metadata=pd.read_csv("metadata",sep="\t")
 
 for s in Super:
- 
     #Gather BioProj_Population 
     ids=metadata[['BioProj_Population']].drop_duplicates()
     ids=ids.reset_index(drop=True)
@@ -18,7 +17,7 @@ for s in Super:
     
     #Read in ancestry inference results 
     results=pd.read_csv(s,sep="\t")
-    results=results.rename(columns = {'ID':'run_accession'})
+
         
     #Merge metadata with results  
     resultsMeta=pd.merge(results,metadata,on=['run_accession'])
@@ -28,30 +27,32 @@ for s in Super:
     acclist=[]
     sample_size=[]
     for i in ids_list:
-       #Calculate accuracy as % 
-       acclist.append(len(resultsMeta[(resultsMeta['BioProj_Population'] == i) & ((resultsMeta['Superpopulation'] == resultsMeta['Eth1']) | (resultsMeta['Superpopulation'] == resultsMeta['Eth2']))]) / len(resultsMeta[resultsMeta['BioProj_Population'] == i])*100)
        #Get sample size
-       sample_size.append(len(resultsMeta[resultsMeta['BioProj_Population'] == i]))
+       sampleSize=len(resultsMeta[resultsMeta['BioProj_Population'] == i])
+       sample_size.append(sampleSize)
+       #Calculate accuracy as % 
+       acclist.append(len(resultsMeta[(resultsMeta['BioProj_Population'] == i) & ((resultsMeta['Superpopulation'] == resultsMeta['Eth1']) | (resultsMeta['Superpopulation'] == resultsMeta['Eth2']))]) / sampleSize*100)
+    
         
         
-        
+          
     
     #Make sample_size into df
     sample_size=DataFrame(sample_size,columns=['Sample Size'])
     
     
     if s.endswith("NeuralNetworkResults"):
-        resultsNN=DataFrame (acclist,columns=['Neural Network Accuracy'])
+        resultsNN=DataFrame(acclist,columns=['Neural Network Accuracy'])
     elif s.endswith("RandomForestResults"):
-        resultsRF=DataFrame (acclist,columns=['Random Forest Accuracy'])      
+        resultsRF=DataFrame(acclist,columns=['Random Forest Accuracy'])      
     else:
-        resultsSVM=DataFrame (acclist,columns=['SVM Accuracy'])
+        resultsSVM=DataFrame(acclist,columns=['SVM Accuracy'])
     
     
-
-
+    
+    
 
 
 
 accResults=pd.concat([ids,sample_size,resultsRF,resultsNN,resultsSVM], axis=1)
-accResults.to_csv('ChrAll_PC20_DPLT5',sep='\t',mode='w',index=False) 
+accResults.to_csv('ChrAll_PC20_DPLT5.tsv',sep='\t',mode='w',index=False) 
