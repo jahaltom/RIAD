1bff12f64570a385767f3455fdbe4594b847b036
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-01-05 19:13:21 -0600

    Update AccuracyTable.py

--- Tools/TestingModel/AccuracyTable.py
+++ Tools/TestingModel/AccuracyTable.py
@@ -3,13 +3,14 @@ from pandas import DataFrame
 
 Super=['SuperpopulationChrAll.PC20SVMResults',  'SuperpopulationChrAll.PC20RandomForestResults',  'SuperpopulationChrAll.PC20NeuralNetworkResults']
 
+
 #Read in metadata
 metadata=pd.read_csv("metadata",sep="\t")
 
 for s in Super:
     #Gather BioProj_Population 
     ids=metadata[['BioProj_Population']].drop_duplicates()
-    ids=ids.reset_index(drop=True)
+    
     
     #Make list
     ids_list=ids['BioProj_Population'].tolist()    
@@ -22,15 +23,22 @@ for s in Super:
     
     acclist=[]
     sample_size=[]
+    numCorrect=0
     for i in ids_list:
        #Get sample size
        sampleSize=len(resultsMeta[resultsMeta['BioProj_Population'] == i])
        sample_size.append(sampleSize)
        #Calculate accuracy as % 
-       acclist.append(len(resultsMeta[(resultsMeta['BioProj_Population'] == i) & ((resultsMeta['Superpopulation'] == resultsMeta['Eth1']) | (resultsMeta['Superpopulation'] == resultsMeta['Eth2']))]) / sampleSize*100)   
+       correct=len(resultsMeta[(resultsMeta['BioProj_Population'] == i) & ((resultsMeta['Superpopulation'] == resultsMeta['Eth1']) | (resultsMeta['Superpopulation'] == resultsMeta['Eth2']))])
+       acclist.append(correct/ sampleSize*100)   
+       
+       #Keep track of total corretly infered. 
+       numCorrect=numCorrect + correct
+       
+    #Overall accuracy across studies
+    acclist.append(numCorrect/sum(sample_size)*100)
     
-    #Make sample_size into df
-    sample_size=DataFrame(sample_size,columns=['Sample Size'])   
+   
     
     if s.endswith("NeuralNetworkResults"):
         resultsNN=DataFrame(acclist,columns=['Neural Network Accuracy'])
@@ -38,6 +46,15 @@ for s in Super:
         resultsRF=DataFrame(acclist,columns=['Random Forest Accuracy'])      
     else:
         resultsSVM=DataFrame(acclist,columns=['SVM Accuracy'])
-        
+
+#Add total sample size to sample_size and make into df
+sample_size.append(sum(sample_size))
+sample_size=DataFrame(sample_size,columns=['Sample Size'])  
+#Add OverallAccuracy string to ids
+ids.loc[-1] = ['OverallAccuracy']
+ids=ids.reset_index(drop=True)
+#Combine results
 accResults=pd.concat([ids,sample_size,resultsRF,resultsNN,resultsSVM], axis=1)
+
+
 accResults.to_csv('ChrAll_PC20_DPLT5.tsv',sep='\t',mode='w',index=False) 
