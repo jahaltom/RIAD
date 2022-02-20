16582e4b58548471f6ab0ab2d473fa38a75b7976
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-02-20 11:30:05 -0600

    Update SomaticVarCall.py

--- SomaticVarCall.py
+++ SomaticVarCall.py
@@ -3,27 +3,29 @@ import pandas as pd
 
 #Create output directory
 DIR='output'
-somatic_calls='Somatic_Calls'
+
 
 #Study design with run accession IDs.
 design=pd.read_csv("design",sep="\t")
 normal=design['Normal'].tolist() 
 tumor=design['Tumor'].tolist() 
+tumor = [x for x in tumor if pd.isnull(x) == False]
 ra=tumor+normal
 
-chr_list = list(range(1, 23))
-chr_list.append("M")
+chr_list = list(range(21, 23))
+#chr_list.append("M")
 
 
+rule all:
+    input: expand("{wd}/{tumorSample}.{chr}.somatic.vcf.gz",wd=DIR,tumorSample=tumor,chr=chr_list)
 
 
 rule BamFormating:
     input:
         expand("{wd}/{sample}/2ndPass.Aligned.sortedByCoord.out.bam",sample=ra,chr=chr_list,wd=DIR)
     output:
-        "{wd}/{sample}/Chr{chr}.filtered.gvcf.gz.tbi"
-    group:
-        "SomaticVarCall"
+        "{wd}/{sample}/Chr{chr}.final.bam"
+
     shell:
         """
         samtools view -b {input} {wildcards.chr}  > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.bam
@@ -47,7 +49,7 @@ rule BamFormating:
 
         gatk SplitNCigarReads \
             -I {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.Grouped.bam  \
-            -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.bam \
+            -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.split.bam \
             -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.Grouped.bam
         
@@ -66,45 +68,70 @@ rule BamFormating:
         """
 
 
-rule CreateSomaticPanelOfNormals:
+rule VarCallNormals:
     input:
-        expand("{wd}/{normalSample}/Chr{wildcards.chr}.final.bam",normalSample=normal,chr=chr_list,wd=DIR,somatic_calls=somatic_calls)
-    output:
+        expand("{wd}/{sample}/Chr{chr}.final.bam",chr=chr_list,wd=DIR,normalSample=normal,sample=ra)
         
-    group:
-        "SomaticVarCall"
+    output:
+        "{wd}/{normalSample}/Chr{chr}.{normalSample}.normal.vcf.gz"
+   
+
     shell:
-        """
+        """   
         gatk Mutect2 \
           -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
-          -I {wildcards.wd}/{wildcards.normalSample}/Chr1.final.bam \
+          -I {wildcards.wd}/{wildcards.normalSample}/Chr{wildcards.chr}.final.bam \
           -tumor {wildcards.normalSample} \
           -L {wildcards.chr} \
           --germline-resource data/Chr{wildcards.chr}_SNPs.vcf.gz \
-          -O {wildcards.wd}/{wildcards.normalSample}/{wildcards.normalSample}.normal.vcf.gz
+          --max-mnp-distance 0 \
+          -O {wildcards.wd}/{wildcards.normalSample}/Chr{wildcards.chr}.{wildcards.normalSample}.normal.vcf.gz
           
-          
-         echo "{wildcards.wd}/{wildcards.normalSample}/{wildcards.normalSample}.normal.vcf.gz" >> {wildcards.wd}/{wildcards.somatic_calls}/normals_for_pon_vcf.args
-         
+          echo $"{wildcards.normalSample}\t{wildcards.wd}/{wildcards.normalSample}/Chr{wildcards.chr}.{wildcards.normalSample}.normal.vcf.gz" >> {wildcards.wd}/normals_for_pon_{wildcards.chr}_vcfs.sample_map
+        """
+  
+rule GenomicsDBImport :
+    input:
+        expand("{wd}/{normalSample}/Chr{chr}.{normalSample}.normal.vcf.gz",chr=chr_list,wd=DIR,normalSample=normal)
+    output:
+        directory("{wd}/pon_{chr}_db")
+    shell:         
+        """       
+         gatk GenomicsDBImport  \
+           -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
+           -L {wildcards.chr} \
+           --genomicsdb-workspace-path {wildcards.wd}/pon_{wildcards.chr}_db \
+           --sample-name-map {wildcards.wd}/normals_for_pon_{wildcards.chr}_vcfs.sample_map
+        """
+        
+
+rule CreateSomaticPanelOfNormals:
+    input:
+         directory(expand("{wd}/pon_{chr}_db",chr=chr_list,wd=DIR))
+    output:
+        "{wd}/pon.{chr}.vcf.gz"
+    shell:         
+        """       
          gatk CreateSomaticPanelOfNormals \
-           -vcfs {wildcards.wd}/{wildcards.somatic_calls}/normals_for_pon_vcf.args \
-           -O {wildcards.wd}/{wildcards.somatic_calls}/pon.vcf.gz
+           -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
+           -V gendb://{wildcards.wd}/pon_{wildcards.chr}_db \
+           -O {wildcards.wd}/pon.{wildcards.chr}.vcf.gz \
+           -L {wildcards.chr}
         """
 
 rule SomaticVarCall:
     input:
-        expand("{wd}/{tumorSample}/Chr{wildcards.chr}.final.bam",tumorSample=tumor,chr=chr_list,wd=DIR,somatic_calls=somatic_calls),
+        expand("{wd}/pon.{chr}.vcf.gz",wd=DIR,tumorSample=tumor,chr=chr_list)
     output:
-        
-    group:
-        "SomaticVarCall"
+        "{wd}/{tumorSample}.{chr}.somatic.vcf.gz"
+
     shell:
         """
         gatk Mutect2 \
           -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
-          -I {wildcards.wd}/{wildcards.tumorSample}/Chr1.final.bam \
-          -L {wildcards.chr}
+          -I {wildcards.wd}/{wildcards.tumorSample}/Chr{wildcards.chr}.final.bam \
+          -L {wildcards.chr} \
           --germline-resource data/Chr{wildcards.chr}_SNPs.vcf.gz \
-          -panel-of-normals {wildcards.wd}/{wildcards.somatic_calls}/pon.vcf.gz \
-          -O {wildcards.wd}/{wildcards.somatic_calls}/{wildcards.tumorSample}.somatic.vcf.gz
+          -panel-of-normals {wildcards.wd}/pon.{wildcards.chr}.vcf.gz \
+          -O {wildcards.wd}/{wildcards.tumorSample}.{wildcards.chr}.somatic.vcf.gz
         """
