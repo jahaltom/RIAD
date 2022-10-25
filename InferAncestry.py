7247b0003c16cb018b73ac502a5ec8bb32ca3112
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-10-25 16:50:12 -0500

    Update InferAncestry.py

--- InferAncestry.py
+++ InferAncestry.py
@@ -5,6 +5,7 @@ from sklearn import metrics
 import random
 from pandas import DataFrame
 import numpy as np
+from os.path import exists
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 import os
@@ -13,6 +14,11 @@ import warnings
 from sklearn.exceptions import ConvergenceWarning
 warnings.simplefilter("ignore", ConvergenceWarning)
 
+#Read in config file
+configfile: "config.yaml"
+Interval = config['Interval']
+PCs=config['PCs']
+OutputDir=config['OutputDir']
 
 #ML methods##############
 
@@ -69,8 +75,7 @@ def neural_network(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
    
     
     test_acc= models[idx].score(xtest,ytest)
-    #Predict unk
-    print(test_acc,max(testacc))
+
     #Predict unk
  
     pred = models[idx].predict(unktest)    
@@ -86,7 +91,7 @@ def neural_network(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
     else: # else it exists so append without writing the header
         pred.to_csv(classification+run+'NeuralNetworkResults',sep='\t',mode='a',index=False,header=None)    
     
-    pred.to_csv(DIR+ "/" + srrid + "/" + classification+run+'NeuralNetworkResults',sep='\t',mode='w',index=False,header=True)    
+    pred.to_csv(OutputDir+ "/" + srrid + "/" + classification+run+'NeuralNetworkResults',sep='\t',mode='w',index=False,header=True)    
 
 def RandomForest_Classifier(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
      
@@ -129,8 +134,7 @@ def RandomForest_Classifier(xtrain,ytrain,xtest,ytest,classification,unktest,srr
             
     testacc= models[idx].score(xtest,ytest)
     
-    #Predict unk
-    print(testacc,max(overalltestacc))
+   
     
     
     pred = models[idx].predict(unktest)    
@@ -147,10 +151,10 @@ def RandomForest_Classifier(xtrain,ytrain,xtest,ytest,classification,unktest,srr
     else: # else it exists so append without writing the header
         pred.to_csv(classification+run+'RandomForestResults',sep='\t',mode='a',index=False,header=None)    
     
-    pred.to_csv(DIR+ "/" + srrid + "/" + classification+run+'RandomForestResults',sep='\t',mode='w',index=False,header=True)
+    pred.to_csv(OutputDir+ "/" + srrid + "/" + classification+run+'RandomForestResults',sep='\t',mode='w',index=False,header=True)
    
 
-#SVMs with Polynomial Kernels
+#SVMs with rbf Kernels
 def svm(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
     max_iter=1000    
     Cvals = np.logspace(-4,2,10)
@@ -197,7 +201,7 @@ def svm(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
     
     pred.insert(len(pred.columns),"TestAcc",[besttestacc],True)
     pred.insert(len(pred.columns),"run_accession",[srrid],True)
-    print(pred,prob)
+   
     
     df = pd.concat([pred, prob], axis=1)
     
@@ -207,12 +211,15 @@ def svm(xtrain,ytrain,xtest,ytest,classification,unktest,srrid,run):
     else: # else it exists so append without writing the header
         df.to_csv(classification+run+'SVMResults',sep='\t',mode='a',index=False,header=None) 
     
-    df.to_csv(DIR+ "/" + srrid + "/" + classification+run+'SVMResults',sep='\t',mode='w',index=False,header=True)
+    df.to_csv(OutputDir+ "/" + srrid + "/" + classification+run+'SVMResults',sep='\t',mode='w',index=False,header=True)
 
-
-    
   
+#N PCs 
+pcs=[]
+for i in range(1,PCs+1):
+    pcs.append("PC"+str(i))
     
+
 def PCA(vcf,metadata_path,run,path,srrid,classification):
         #Import metadata and gather sample names for each person in 1KGP.
         metadata=pd.read_csv(metadata_path,sep='\t')
@@ -247,11 +254,25 @@ def PCA(vcf,metadata_path,run,path,srrid,classification):
         train.to_csv(path+"train", sep=" ",header=False,index=False)
         
         
-        #Run PLINK PCA
-        shell("plink --vcf " + vcf + " --pca 20 --within " + path + "samples --mac 1 --pca-clusters " + path + "train --out " + path + run)
+        #shell("plink --vcf " + vcf + " --pca "+str(PCs)+" --within " + path + "samples --mac 1 --pca-clusters " + path + "train --out " + path + run)
+        #Run PLINK PCA     
+        plink=open(path +"plink.sh", "w")   
+        plink.write("#!/bin/bash")
+        plink.write('\n')
+        plink.write("#SBATCH --time=1:00:00   # walltime limit (HH:MM:SS)")
+        plink.write('\n')
+        plink.write("#SBATCH --ntasks-per-node=7")
+        plink.write('\n')
+        plink.write("#SBATCH -p RM-shared")
+        plink.write('\n')
+        plink.write("source activate Ancestry")
+        plink.write('\n')
+        plink.write("plink --vcf " + vcf + " --pca "+str(PCs)+" --within " + path + "samples --mac 1 --pca-clusters " + path + "train --out " + path + run)
+        plink.close()
+        subprocess.run("bash "+path +"plink.sh", shell=True, check=True) 
         
         #PCA data from plink
-        vardata= pd.read_csv(path + run + '.eigenvec',delimiter=r"\s+",names=['Drop','Sample name','PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20'])
+        vardata= pd.read_csv(path + run + '.eigenvec',delimiter=r"\s+",names=['Drop','Sample name']+pcs)
         vardata=vardata.drop(['Drop'], axis=1)
         
         
@@ -264,57 +285,57 @@ def PCA(vcf,metadata_path,run,path,srrid,classification):
         
         #Make unkown, training, and test sets for ML methods.
         Train = pd.merge(data,train,on=['Sample name'])
-        x_train=Train[['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20']]  # Features
+        x_train=Train[pcs]  # Features
         y_train=Train[classification + ' code']  # Labels
         
         Test = pd.merge(data,test,on=['Sample name'])
-        x_test=Test[['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20']]  # Features
+        x_test=Test[pcs]  # Features
         y_test=Test[classification + ' code']  # Labels
         
         
         
         unk_test = vardata[vardata['Sample name'] == "sample" ]
-        unk_test=unk_test[['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20']]
+        unk_test=unk_test[pcs]
       
     
             
         svm(x_train,y_train,x_test,y_test,classification,unk_test,srrid,run)
-        #RandomForest_Classifier(x_train,y_train,x_test,y_test,classification,unk_test,srrid,run)
-        #neural_network(x_train,y_train,x_test,y_test,classification,unk_test,srrid,run)
+        RandomForest_Classifier(x_train,y_train,x_test,y_test,classification,unk_test,srrid,run)
+        neural_network(x_train,y_train,x_test,y_test,classification,unk_test,srrid,run)
         
 #####################
 
         
 #Create output directory
-DIR='output'
+
 
 
 #Read in run accession IDs from txt file.
-with open ("RAids.txt") as f:
-    ra=f.read().splitlines()
+with open ("ids.txt") as f:
+    id=f.read().splitlines()
 
-test="ChrAll.PC20"
+chrPCSpec="Chr"+str(Interval).replace("[", "").replace("]", "").replace(",", "_").replace(" ", "")+".PC"+str(PCs)
 
-chr_list = list(range(1, 23))
+if Interval == "All":
+    chr_list = list(range(1, 23))
+else:
+    chr_list=Interval
+
+
 
 
 rule all:
-    input: expand("{wd}/{sample}/Superpopulation{test}SVMResults",sample=ra,wd=DIR,test=test)
+    input: expand("{wd}/{sample}/Superpopulation{chrPCSpec}SVMResults",sample=id,wd=OutputDir,chrPCSpec=chrPCSpec)
 
 
-rule var_call:
+rule GATK:
     input:
         "{wd}/{sample}/2ndPass.Aligned.sortedByCoord.out.bam"
     output:
-        "{wd}/{sample}/Chr{chr}.final.vcf.gz.tbi"
-    group:
-        "Genotype"
+        "{wd}/{sample}/Chr{chr}.{sample}.vcf.gz.tbi"
     shell:
         """       
         samtools view -b {input} {wildcards.chr}  > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.bam
-
-
-
         gatk MarkDuplicates \
             I= {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.bam \
             O= {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.MarkDup.bam \
@@ -322,7 +343,6 @@ rule var_call:
             METRICS_FILE= {wildcards.wd}/{wildcards.sample}/marked_dup_metrics.{wildcards.chr}.txt \
             VALIDATION_STRINGENCY= SILENT
         rm {wildcards.wd}/{wildcards.sample}/*Chr{wildcards.chr}.bam
-
         gatk AddOrReplaceReadGroups \
             I= {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.MarkDup.bam \
             O= {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.Grouped.bam \
@@ -331,15 +351,11 @@ rule var_call:
             RGPL= plat \
             RGPU= plat
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.MarkDup*
-
         gatk SplitNCigarReads \
             -I {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.Grouped.bam  \
             -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.bam \
             -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.Grouped.bam
-
-
-
         gatk --java-options "-Xmx20g" HaplotypeCaller \
            -I {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.bam \
            -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.vcf.gz \
@@ -348,29 +364,17 @@ rule var_call:
            -ERC GVCF \
            --native-pair-hmm-threads 7
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final*
-
-
         gunzip -c {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.vcf.gz |  grep -v "0/0:0:0:0:0,0,0" | bgzip > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.vcf.gz
         rm {wildcards.wd}/{wildcards.sample}/*Chr{wildcards.chr}.vcf.gz
-
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.vcf.gz
-        """
-rule Genotype:
-    input: 
-        "{wd}/{sample}/Chr{chr}.final.vcf.gz.tbi"
-    output: 
-        "{wd}/{sample}/Chr{chr}.final2.vcf.gz.tbi"
-    group: 
-        "Genotype"
-    shell:
-        """
+
+
         gatk --java-options "-Xmx10g" GenotypeGVCFs \
         -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
         -V {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final.vcf.gz \
         -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.vcf.gz \
         --include-non-variant-sites
         
-
         gatk --java-options "-Xmx10g" VariantFiltration \
         -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
         -V {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.vcf.gz \
@@ -388,54 +392,32 @@ rule Genotype:
         --filter-expression "QD < 2.0" \
         --filter-name "filter_QD"
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.vcf.gz*
-
-
         gatk --java-options "-Xmx10g" SelectVariants \
         -R data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna \
         -V {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.filtered.vcf.gz  \
         -O {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.vcf.gz \
         --exclude-filtered true \
         --select-type-to-exclude INDEL
-
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.filtered.vcf.gz*
-        """
-rule compare:
-    input: 
-        "{wd}/{sample}/Chr{chr}.final2.vcf.gz.tbi"
-    output: 
-        "{wd}/{sample}/Chr{chr}.{sample}.vcf.gz.tbi"
-    group:               
-        "Genotype"
-    shell:
-        """
+
 
         gunzip -c  {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.vcf.gz | grep -v  "\./\." | bgzip > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.filtered.vcf.gz
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.filtered.vcf.gz
-
         
-
         #all sites with matching positions. Will result in all 0/0 calls that match and will have 0/1 and 1/1 which also match but some will be the wronf alt allele.
-
         tabix -h -T data/Chr{wildcards.chr}_SNPs.bed {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.filtered.vcf.gz | bgzip  > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz
-
-
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.final2.filtered.vcf.gz*
-
         #Make an index file of comm_pos.vcf.gz. Handels blank vcfs
         gunzip -c {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz | {{ grep -v "#" || true; }} | awk -F'\t' -v OFS='\t' '{{print $1,$2}}' | cat > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.interval_list
-
         #Extract positions from 1KGP
         tabix -h -R {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.interval_list  data/Chr{wildcards.chr}_SNPs.vcf.gz | bgzip   > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz
-
         #Match only on positions that have the same allele.
         bcftools isec -c none {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz --output-type z --threads 7 -p {wildcards.wd}/{wildcards.sample}/dir{wildcards.chr}
         #0000.vcf.gz is records private to  comm_pos.vcf.gz.They will be 0/0 calls that are true matches and 1/1 and 0/1 calls with wrong alt allele. Remove 0/0 and same others as an index list.
         gunzip -c {wildcards.wd}/{wildcards.sample}/dir{wildcards.chr}/0000.vcf.gz | grep -v "0/0" | {{ grep -v "#" || true; }} | awk -F'\t' -v OFS='\t' '{{print $1,$2}}' | cat > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}List
-
         #If List is not empty, then remove unwanted SNPs, else remove nothing.
-
         if test -s {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}List ; then
                 bcftools view -T ^{wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}List {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz --output-type z --threads 7 > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.1KG.vcf.gz
                 bcftools view -T ^{wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}List {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz --output-type z --threads 7 > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.unk.vcf.gz
@@ -443,52 +425,44 @@ rule compare:
                 mv {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.1KG.vcf.gz
                 mv {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.unk.vcf.gz
         fi
-
         rm -r  {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.interval_list  {wildcards.wd}/{wildcards.sample}/dir{wildcards.chr} {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}1000.vcf.gz* {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.comm_pos.vcf.gz* {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}List
-
         #Use List to remove unmatched positions from data comtaining all matches 0/0 1/0 1/1.
-
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.unk.vcf.gz
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.1KG.vcf.gz
         #Combine 1KGP with sample to have ancestry infered
-
+        
         bcftools merge {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.1KG.vcf.gz {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.unk.vcf.gz  --output-type z --threads 7 > {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.{wildcards.sample}.vcf.gz
         bcftools index -t {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.{wildcards.sample}.vcf.gz
-
         rm {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.1KG.vcf.gz* {wildcards.wd}/{wildcards.sample}/Chr{wildcards.chr}.unk.vcf.gz*
-
-        """
-
-rule concat:
-    input: 
-        expand("{wd}/{sample}/Chr{chr}.{sample}.vcf.gz.tbi",sample=ra,chr=chr_list,wd=DIR)
-    output: 
-        "{wd}/{sample}/All.{sample}.vcf.gz"
-    group:
-        "Infer Ancestry"
-    shell:
-        """
-        #rm {wildcards.wd}/{wildcards.sample}/2ndPass.Aligned.sortedByCoord.out.bam*
+        
         rm {wildcards.wd}/{wildcards.sample}/*.final.vcf.gz*
         rm {wildcards.wd}/{wildcards.sample}/*.final2.vcf.gz*
-        bcftools concat {wildcards.wd}/{wildcards.sample}/*{wildcards.sample}.vcf.gz --output-type z --threads 7 > {wildcards.wd}/{wildcards.sample}/All.{wildcards.sample}.vcf.gz
         """
+        
+rule concat:
+    input: 
+        expand("{wd}/{sample}/Chr{chr}.{sample}.vcf.gz.tbi",sample=id,chr=chr_list,wd=OutputDir)
+    output: 
+        "{wd}/{sample}/"+chrPCSpec.replace(".PC"+str(PCs),"_")+".{sample}.vcf.gz"
 
+    run:
+       
+        
+        shell("bcftools concat "+str(input).replace(".tbi", "")+" --output-type z --threads 7 > {wildcards.wd}/{wildcards.sample}/"+chrPCSpec.replace(".PC"+str(PCs),"_")+".{wildcards.sample}.vcf.gz")
+   
 rule SuperPop:
-    input: "{wd}/{sample}/All.{sample}.vcf.gz"
-    
+    input: "{wd}/{sample}/"+chrPCSpec.replace(".PC"+str(PCs),"_")+".{sample}.vcf.gz"
+
     output:
-        "{wd}/{sample}/Superpopulation{test}SVMResults"
-    group:
-        "Infer Ancestry"
+        "{wd}/{sample}/Superpopulation{chrPCSpec}SVMResults"
+
     run:
    
         srrid=str(output).split("/")[1]
-        path=DIR + "/"+srrid + "/"
+        path=OutputDir + "/"+srrid + "/"
         vcf=str(input)
         metadata_path="data/1KGP.metadata.tsv"    
         
         #All is ChrAll,Chr#,ect
-        PCA(vcf,metadata_path,test,path,srrid,"Superpopulation")
-
-        
+        PCA(vcf,metadata_path,chrPCSpec,path,srrid,"Superpopulation")
+        shell("rm {wildcards.wd}/{wildcards.sample}/plink.sh")
