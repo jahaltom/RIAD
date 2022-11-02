e239802e67f6f299e8d1d5f951ecf0653cb97414
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-11-02 18:59:01 -0500

    Update masker.sh

--- other/Masking/masker.sh
+++ other/Masking/masker.sh
@@ -7,7 +7,7 @@ percentualHead() {
 
 
 #Extract SAMPLE from 1KGP-SAMPLE vcf
-bcftools view -s SAMPLE --threads 7 --output-type z OUTPUT/SAMPLE/ChrAll_.SAMPLE.vcf.gz > OUTPUT/SAMPLE/SAMPLE.vcf.gz
+bcftools view -s SAMPLE --threads THREADS --output-type z OUTPUT/SAMPLE/ChrAll_.SAMPLE.vcf.gz > OUTPUT/SAMPLE/SAMPLE.vcf.gz
 
 #Gather coordinates for all HomRef calls and randomly suffel. Take 5% for targets.
 bcftools query -f'[%CHROM:%POS:%REF:%ALT\n]' -i'GT="RR"' OUTPUT/SAMPLE/SAMPLE.vcf.gz   > OUTPUT/SAMPLE/HomRef
@@ -25,7 +25,7 @@ rm OUTPUT/SAMPLE/cords
 cat  OUTPUT/SAMPLE/targets | sed 's/:/ /g' | awk '{print $1"\t"$2}'  > OUTPUT/SAMPLE/targets_file
 
 #create id in SAMPLE vcf
-bcftools annotate --set-id '%CHROM\:%POS\:%REF\:%ALT' --threads 7 OUTPUT/SAMPLE/SAMPLE.vcf.gz > OUTPUT/SAMPLE/SAMPLE.vcf
+bcftools annotate --set-id '%CHROM\:%POS\:%REF\:%ALT' --threads THREADS OUTPUT/SAMPLE/SAMPLE.vcf.gz > OUTPUT/SAMPLE/SAMPLE.vcf
 rm OUTPUT/SAMPLE/SAMPLE.vcf.gz
 
 #Gather targets and non target vcf.
@@ -52,16 +52,16 @@ grep "#" OUTPUT/SAMPLE/targets.vcf > OUTPUT/SAMPLE/header
 cat OUTPUT/SAMPLE/targets.vcf  OUTPUT/SAMPLE/non-targets.vcf | grep -v "#" | sort -k1,1 -k2,2n > OUTPUT/SAMPLE/variants
 cat OUTPUT/SAMPLE/header OUTPUT/SAMPLE/variants   | bgzip > OUTPUT/SAMPLE/Temp_ChrAll_Noise.SAMPLE.vcf.gz
 #Remove INFO. Otherwise will mess up merge.
-bcftools annotate -x ID,INFO --threads 7  --output-type z OUTPUT/SAMPLE/Temp_ChrAll_Noise.SAMPLE.vcf.gz  > OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz
+bcftools annotate -x ID,INFO --threads THREADS  --output-type z OUTPUT/SAMPLE/Temp_ChrAll_Noise.SAMPLE.vcf.gz  > OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz
 bcftools index -t OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz
 rm OUTPUT/SAMPLE/header OUTPUT/SAMPLE/variants OUTPUT/SAMPLE/targets.vcf  OUTPUT/SAMPLE/non-targets.vcf OUTPUT/SAMPLE/Temp_ChrAll_Noise.SAMPLE.vcf.gz
 
 
 #Remove SAMPLE from 1KGP-SAMPLE vcf
-bcftools view -s ^SAMPLE --threads 7  --output-type z OUTPUT/SAMPLE/ChrAll_.SAMPLE.vcf.gz  > OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz
+bcftools view -s ^SAMPLE --threads THREADS  --output-type z OUTPUT/SAMPLE/ChrAll_.SAMPLE.vcf.gz  > OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz
 bcftools index -t OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz
 
-bcftools merge OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz --output-type z --threads 7 > OUTPUT/SAMPLE/ChrAll.SAMPLE.Noise.vcf.gz
+bcftools merge OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz --output-type z --threads THREADS > OUTPUT/SAMPLE/ChrAll.SAMPLE.Noise.vcf.gz
 bcftools index -t OUTPUT/SAMPLE/ChrAll.SAMPLE.Noise.vcf.gz
 rm OUTPUT/SAMPLE/ChrAll_Noise.SAMPLE.vcf.gz* OUTPUT/SAMPLE/ChrAll_.1KGP.SAMPLE.vcf.gz*
 
