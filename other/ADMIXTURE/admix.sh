21a10aef3368c71ab376d097a7fb8af85ea97c78
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-11-03 18:09:53 -0500

    Update admix.sh

--- other/ADMIXTURE/admix.sh
+++ other/ADMIXTURE/admix.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --time=4:00:00   # walltime limit (HH:MM:SS)
 #SBATCH -p RM-shared
-#SBATCH -c 4
+#SBATCH -c 6
 
 source activate Ancestry
 
@@ -10,7 +10,7 @@ cp 1KGP.pop output/SAMPLE/
 cd output/SAMPLE/
 #plink --vcf ChrAll.SAMPLE.Noise.vcf.gz --make-bed --out 1KGP
 plink --vcf ChrAll_.SAMPLE.vcf.gz --make-bed --out 1KGP
-/ocean/projects/bio210061p/jahaltom/tools/dist/admixture_linux-1.3.0/admixture 1KGP.bed 5 --supervised -j4
+/ocean/projects/bio210061p/jahaltom/tools/dist/admixture_linux-1.3.0/admixture 1KGP.bed 5 --supervised -j6
 tail -1 1KGP.5.Q > SAMPLE.prop
 
 python  SAMPLE.py
