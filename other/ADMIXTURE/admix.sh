6788dada1bb150f5ae2b41693839594950ebdd69
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-11-04 13:29:51 -0500

    Update admix.sh

--- other/ADMIXTURE/admix.sh
+++ other/ADMIXTURE/admix.sh
@@ -8,8 +8,8 @@ source activate Ancestry
 
 cp 1KGP.pop output/SAMPLE/
 cd output/SAMPLE/
-plink --vcf ChrAll.SAMPLE.Noise.vcf.gz --make-bed --out 1KGP
-#plink --vcf ChrAll_.SAMPLE.vcf.gz --make-bed --out 1KGP
+#plink --vcf ChrAll.SAMPLE.Noise.vcf.gz --make-bed --out 1KGP
+plink --vcf ChrAll_.SAMPLE.vcf.gz --make-bed --out 1KGP
 /ocean/projects/bio210061p/jahaltom/tools/dist/admixture_linux-1.3.0/admixture 1KGP.bed 5 --supervised 
 tail -1 1KGP.5.Q > SAMPLE.prop
 
