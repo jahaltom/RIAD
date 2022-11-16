a6fd7cc1f21a62114ff5eb19259ac03378611306
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-11-16 08:11:35 -0600

    Update AllVar.py

--- AllVar.py
+++ AllVar.py
@@ -11,7 +11,8 @@ with open ("RAids.txt") as f:
 
 
 if Interval == "All":
-    chr_list=list(range(1, 23))
+    for i in range(1, 23):
+        chr_list.append(str(i))
 else:
     chr_list=Interval
     
